607dabc698d795f36391011630716b454c969657
Werner <werner@toeps.co.za>
2022-10-05 21:01:39 +0200

    color changes

--- stockManagement/src/app/pages/admin/create-new/create-new.component.ts
+++ stockManagement/src/app/pages/admin/create-new/create-new.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+
 import { Router } from '@angular/router';
+import { Vehicle } from '../../../components/data-table/data-table.component';
+import { DataService } from '../../../services/data.service';
 
 @Component({
   selector: 'app-create-new',
@@ -8,11 +10,44 @@ import { Router } from '@angular/router';
   styleUrls: ['./create-new.component.scss'],
 })
 export class CreateNewComponent implements OnInit {
-  constructor(private router: Router) {}
+  vehicle: Vehicle;
+  newVehicle: Vehicle;
+
+  constructor(private router: Router, private dataService: DataService) {}
 
   save() {
     this.router.navigate(['admin', 'dashboard']);
+    this.dataService.saveVehicle(this.newVehicle);
+  }
+  back() {
+    this.router.navigate(['admin', 'dashboard']);
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.newVehicle = {
+      RegNo: '',
+      Make: '',
+      Model: '',
+      ModelYear: 0,
+      Kilometers: 0,
+      Colour: '',
+      VIN: '',
+      RetailPrice: 0,
+      CostPrice: 0,
+      Accessories: [
+        {
+          name: '',
+          description: '',
+        },
+      ],
+      images: [
+        {
+          src: '',
+        },
+      ],
+
+      DateCreated: new Date(),
+      DateUpdated: new Date(),
+    };
+  }
 }
